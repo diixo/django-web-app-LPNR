ea572386b28ebfad21a1e112457f0a8c2a7339c6
diixo <anatolii.podoba@gmail.com>
2024-04-13 21:14:02 +0300

    initial commit app-ua.py + trained model + xml

--- core/core/settings.py
+++ core/core/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+ML_ROOT = BASE_DIR / 'ml'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
